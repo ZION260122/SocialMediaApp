3526726c76e7bca6e9ad882089f2502519ca9fc2
utkarsh <98471140+ZION260122@users.noreply.github.com>
2024-08-08 18:05:25 +0530

    chat app backend API

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,7 +1,7 @@
-import Conversation from "../models/conversationMode";
-import Message from "../models/messageModel";
+import Conversation from "../models/conversationMode.js";
+import Message from "../models/messageModel.js";
 
-const sendMessage = async (req, res) => {
+async function sendMessage (req, res) {
     try {
         const {recipientId, message} = req.body;
         const senderId = req.user._id;
@@ -26,7 +26,7 @@ const sendMessage = async (req, res) => {
             sender: senderId,
             text: message,
          });
-         await promise.all([
+         await Promise.all([
             newMessage.save(),
             conversation.updateOne({
                 lastMessage: {
@@ -44,4 +44,50 @@ const sendMessage = async (req, res) => {
     }
 }
 
-export {sendMessage}
+async function getMessages(req, res) {
+        const {otherUserId} = req.params;
+        const userId = req.user._id;
+    try {
+        const conversation = await Conversation.findOne({
+            participants: { $all: [userId, otherUserId] }
+        })
+
+        if(!conversation) {
+            return res.status(404).json({ error: "Conversation not found"});
+        }
+
+        const messages = await Message.find({
+            conversationId : conversation._id
+        }).sort({createdAt: 1})
+
+        res.status(200).json(messages);
+        
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+
+}
+
+import mongoose from 'mongoose';
+
+async function getConversations(req, res) {
+    const userId = req.user._id;
+
+    try {
+
+        const conversations = await Conversation.find({
+            participants: userId
+        }).populate({
+            path: "participants",
+            select: "username profilePic"
+        });
+
+        res.status(200).json(conversations);
+        
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+
+export {sendMessage, getMessages, getConversations}
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,9 +1,11 @@
 import express from "express"
-import protectRoute from "../middlewares/protectRoute";
-import { sendMessage } from "../controllers/messageController";
+import protectRoute from "../middlewares/protectRoute.js";
+import { getMessages, getConversations, sendMessage } from "../controllers/messageController.js";
 
-const router = express.router()
+const router = express.Router()
 
-router.get('/',protectRoute, sendMessage)
+router.post('/',protectRoute, sendMessage)
+router.get('/:otherUserId',protectRoute, getMessages)
+router.get('/:conversations',protectRoute, getConversations)
 
 export default router;
